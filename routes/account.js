a4d9be6d0858e3abf1a6d86654e7dc5a787aedca
John Li <litiany892@students.itu.edu>
2017-12-02 16:07:09 -0800

    Add some pages: log out, news, forget password and update profile.

--- routes/account.js
+++ routes/account.js
@@ -41,6 +41,23 @@ module.exports = (router) => {
 
   router.get('/logout', (req, res) => {
     req.logout();
-    res.redirect('/');
+    //res.redirect('/');
+    res.render('logout', {});
+  });
+
+  router.get('/update_profile', (req, res, next) => {
+    if (!req.user) {
+      console.log('Forbidden');
+      var err = new Error('Forbidden');
+      err.status = 403;
+      next(err);
+      return;
+    }
+    res.render('update_profile', {});
+  });
+
+  router.get('/forget_password', (req, res, next) => {
+    console.log(req.body.username);
+    res.render('reset_password', {});
   });
 }
--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,23 @@ const path = require('path');
 const databaseAdapter = require('./../libs/database_adapter');
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function cleanJson(data) {
+  const cleanedJson = data.map((item) => {
+    return {
+      Category: item.Category,
+      Headline: item.Headline,
+      URL: item.URL,
+      ReporterName: item.ReporterName,
+      PubTime: item.PubTime,
+      Source: item.Source,
+      Paragraph1: item.Paragraph1,
+      Paragraph2: item.Paragraph2,
+    }
+  });
+  return cleanedJson;
 }
 
 module.exports = (router) => {
@@ -10,18 +26,7 @@ module.exports = (router) => {
   topicNames.forEach((topicName) => {
     router.get('/' + topicName , (req, res) => {
       databaseAdapter.viewNews().then((output) => {
-        const normalizedOutput = output.map((item) => {
-          return {
-            Category: item.Category,
-            Headline: item.Headline,
-            URL: item.URL,
-            ReporterName: item.ReporterName,
-            PubTime: item.PubTime,
-            Source: item.Source,
-            Paragraph1: item.Paragraph1,
-            Paragraph2: item.Paragraph2,
-          }
-        });
+        const normalizedOutput = cleanJson(output);
         const filteredOutput = normalizedOutput.filter((item) => {
           return (item.Category === capitalizeFirstLetter(topicName));
         });
@@ -32,6 +37,34 @@ module.exports = (router) => {
           news: filteredOutput,
         });
       });
+    });
+  });
+
+  router.get('/news', (req, res, next) => {
+    console.log("ENTER");
+    databaseAdapter.viewNews().then((output) => {
+      const normalizedOutput = cleanJson(output);
+      const selectedTopics = ['politics', 'health', 'entertainment'];
+      if (selectedTopics.length !== 3) {
+        console.log('Number of topics is not 3.');
+        var err = new Error('Number of topics is not 3.');
+        err.status = 500;
+        next(err);
+      } else {
+        const categoriedOutput = selectedTopics.map((topicName) => {
+          return {
+            topic: topicName,
+            topic_first_letter_uppercased: capitalizeFirstLetter(topicName),
+            news: normalizedOutput.filter((item) => {
+              return (item.Category === capitalizeFirstLetter(topicName));
+            }),
+          }
+        });
+        res.render('news', {
+          user: req.user,
+          categoried_news: categoriedOutput,
+        });
+      }
     });
   });
 }
